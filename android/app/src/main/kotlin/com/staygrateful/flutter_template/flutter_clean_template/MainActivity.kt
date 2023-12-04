290034c893fb25543330426ed47e1d06a992953e
Riky Ahmad Fathoni <riky.fathoni@gmail.com>
2023-12-04 13:32:46 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/staygrateful/flutter_template/flutter_clean_template/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.staygrateful.flutter_template.flutter_clean_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/color.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+/// System
+const colorPrimary = Color(0xFFE8F9FD);
+const colorTertiary = Color(0xFF5ACAFA);
+const colorNeutral = Color(0xFF939094);
+
+/// Widget
+const colorToolbar = Color(0xFFE8F9FD);
+const colorToolbarDark = Color(0xFF5ACAFA);
+const colorIconBlue = Color(0xFF5ACAFA);
+
+/// Text
+const colorText = Color(0xFF2D2D2D);
+const colorTextDark = Color(0xFFFFFFFF);
